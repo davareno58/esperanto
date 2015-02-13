116d7c45d782236daa5d07988404ed676f812a38
davareno58 <davareno58@gmail.com>
2015-02-13 14:59:24 -0800

    Initialize repository

---
+++ gen/davidkennethcrandall/esperantoanywhere/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package davidkennethcrandall.esperantoanywhere;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
